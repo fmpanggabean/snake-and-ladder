abb83bda0ad58458406b358e92a0599442e82234
F.M. Panggabean <cichio.maru@gmail.com>
2022-07-20 14:20:44 +0700

    menambah world generator
    world dan editor untuk generate block, otomatis set next block ke block yang baru

---
+++ Assets/Apps/Scripts/SnakeAndLadder/Gameplay/Block.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeAndLadder.Gameplay {
+    public class Block : MonoBehaviour {
+        [SerializeField] private Block nextBlock;
+
+        internal void SetNextBlock(Block block) {
+            nextBlock = block;
+        }
+
+        internal Vector3 GetPosition() {
+            return transform.position;
+        }
+
+        internal void SetPosition(Vector3 position) {
+            transform.position = position;
+        }
+    } 
+}
